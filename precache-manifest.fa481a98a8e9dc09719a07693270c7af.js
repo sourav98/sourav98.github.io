cb0d4e76838118e7d32ede5d923dd63367435065
Sourav Sudhi <sourav.sudhi@gmail.com>
2020-05-28 11:29:04 +0530

    Updates

---
+++ precache-manifest.fa481a98a8e9dc09719a07693270c7af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7b99f8a93ba36956fca0bfee4c198c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1266ab7f487a03c1ceab",
+    "url": "/static/css/main.281a3449.chunk.css"
+  },
+  {
+    "revision": "20494c80e2e9165ec900",
+    "url": "/static/js/2.1b8ae30f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b8ae30f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1266ab7f487a03c1ceab",
+    "url": "/static/js/main.7b927df9.chunk.js"
+  },
+  {
+    "revision": "24f87387beb88594118a",
+    "url": "/static/js/runtime-main.a34030d8.js"
+  },
+  {
+    "revision": "abbafa90d25110a4b57f49d3619d20af",
+    "url": "/static/media/logo.abbafa90.svg"
+  },
+  {
+    "revision": "73386caefeeeccf321204a09e4beb6f0",
+    "url": "/static/media/souravsvg.73386cae.svg"
+  }
+]);
